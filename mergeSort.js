09a5f7a748284aed909338b519d7ee19976a58ff
NesryaAbdulkadir <nesryaabdulkadir.dev@gmail.com>
2025-01-13 09:08:26 +0300

    Add Merge Sort Function using Recursion

---
+++ mergeSort.js
@@ -0,0 +1,23 @@
+function merge(left, right) {
+  let result = [],
+    leftIndex = 0,
+    rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+  let middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
+}
+console.log(mergeSort([105, 79, 100, 110]));
