376a4560a88ff44c94d07fb896230964c247f8a7
NesryaAbdulkadir <nesryaabdulkadir.dev@gmail.com>
2025-01-13 08:43:48 +0300

    Fibonacciiteration method

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-function fibs(number) {}
+function fibs(number) {
+  let fib = [0, 1];
+  for (let i = 2; i < number; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  console.log(fib.slice(0, number));
+}
+fibs(2);
